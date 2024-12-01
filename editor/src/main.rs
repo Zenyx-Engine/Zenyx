6ee866a30e4590821bc485b6073dc52e9081a505
Caznix <caznix01@gmail.com>
2024-12-01 18:06:27 -0500

    update workflow

--- editor/src/main.rs
+++ editor/src/main.rs
@@ -1,18 +1,3 @@
-use floem::{prelude::*, style::Style};
-
 fn main() {
-    floem::launch(counter_view);
-}
-
-fn counter_view() -> impl IntoView {
-    let mut counter = RwSignal::new(20);
-    v_stack({
-        label("test")
-    }).style(Style::d);
-    h_stack((
-        button("Increment").action(move || counter += 1),
-        label(move || format!("Value: {counter}")),
-        button("Decrement").action(move || counter -= 1),
-    ))
-    .style(|s| s.size_full().items_center().justify_center().gap(10))
+    println!("editor")
 }
