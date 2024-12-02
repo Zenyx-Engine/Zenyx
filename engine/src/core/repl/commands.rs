96fb6e5747d1f78258876b67f035d0530abdd91d
Jason Spalti <vipershniper08@gmail.com>
2024-12-02 04:58:23 -0600

    Refactor logging to switch between stdout and file logging

--- engine/src/core/repl/commands.rs
+++ engine/src/core/repl/commands.rs
@@ -1,28 +1,26 @@
 use super::COMMAND_LIST;
-use log::{debug, info};
 use std::process::Command;
 
 pub(crate) fn say_hello() {
-    info!("Hello, World!");
+    println!("Hello, World!");
 }
 
 pub(crate) fn echo(args: Vec<String>) {
-    debug!("{}", args.join(" "));
-    info!("{}", args.join(" "))
+    println!("{}", args.join(" "))
 }
 
 pub(crate) fn exit() {
-    debug!("Exiting...");
+    println!("Exiting...");
     std::process::exit(0)
 }
 
 pub(crate) fn clear() {
-    info!("Clearing screen..., running command");
+    println!("Clearing screen..., running command");
     let _result = if cfg!(target_os = "windows") {
-        debug!("target_os is windows");
+        dbg!("target_os is windows");
         Command::new("cmd").args(["/c", "cls"]).spawn()
     } else {
-        debug!("target_os is unix");
+        dbg!("target_os is unix");
         // "clear" or "tput reset"
         Command::new("tput").arg("reset").spawn()
     };
