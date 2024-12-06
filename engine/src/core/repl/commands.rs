4d2383d54ca190c50fa938bdb791953849465bd7
Caznix <caznix01@gmail.com>
2024-12-06 16:20:16 -0500

    fix tokio runtime stack overflow due to recursion

--- engine/src/core/repl/commands.rs
+++ engine/src/core/repl/commands.rs
@@ -1,12 +1,18 @@
 use std::{ffi::OsStr, process::Command};
 
 
-
+use parking_lot::Mutex;
+use lazy_static::lazy_static;
 
 
 use crate::core::repl::repl::evaluate_command;
 
 use super::COMMAND_LIST;
+const MAX_RECURSION_DEPTH: usize = 500; // increasing this value WILL cause a stack overflow. attempt at your own risk - Caz
+
+lazy_static! {
+    static ref RECURSION_DEPTH: Mutex<usize> = parking_lot::Mutex::new(0);
+}
 
 pub(crate) fn say_hello() -> anyhow::Result<()> {
     println!("Hello, World!");
@@ -41,6 +47,13 @@ pub(crate) fn help() -> anyhow::Result<()> {
     Ok(())
 }
 pub(crate) fn exec(args: Vec<String>) -> anyhow::Result<()> {
+    *RECURSION_DEPTH.lock() += 1;
+    if *RECURSION_DEPTH.lock() > MAX_RECURSION_DEPTH {
+        eprintln!("Maximum recursion depth reached. Aborting.");
+        *RECURSION_DEPTH.lock() = 0;
+        return Ok(());
+    }
+    println!("Recursion depth: {}", *RECURSION_DEPTH.lock());
     let file_path_str = &args[0];
     let file_path = std::path::Path::new(file_path_str);
     println!("File path: {:#?}", file_path);
