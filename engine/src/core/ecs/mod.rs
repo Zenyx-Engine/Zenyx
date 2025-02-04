894203be53fadbf8d9e9eb64329b66bcb341e692
Caznix <caznix01@gmail.com>
2025-02-04 05:10:53 -0500

    cube

--- engine/src/core/ecs/mod.rs
+++ engine/src/core/ecs/mod.rs
@@ -1,3 +1,15 @@
-// struct ComponentRegistry {
-//     components
-// }
+
+
+pub trait Component: Sized + 'static {
+    fn update(&mut self, delta_time: f32);
+    fn serialize(&self) -> Vec<u8>;
+    fn deserialize(data: &[u8;6]) -> Self;
+}
+
+pub trait Entity: Sized {
+    fn add_component<C: Component>(&mut self, component: C);
+    fn remove_component<C: Component>(&mut self);
+    fn get_component<C: Component>(&self) -> Option<&C>;
+    fn serialize(&self) -> Vec<u8>;
+    fn deserialize(data: &[u8;6]) -> Self;
+}
